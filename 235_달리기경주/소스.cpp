9fbfd884c85d79ecf90f397bd9f5ef9f0aeb7a68
seongtaekoh <post4204@naver.com>
2023-04-10 17:50:41 +0900

    Programmers_Level1_Solution

---
+++ 235_달리기경주/소스.cpp
@@ -0,0 +1,102 @@
+#include <string>
+#include <map>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> players, vector<string> callings) 
+{
+    vector<string> answer;
+   
+    map<int, string> m2;
+    map<string, int> m;
+    map<int, string>::iterator iter2 = m2.begin();
+    map<string, int>::iterator iter = m.begin();
+
+    for (int i = 0; i < players.size(); i++)
+    {
+        m.insert(make_pair(players[i], i));
+        m2.insert(make_pair(i, players[i]));
+    }
+    
+    for (int i = 0; i < callings.size(); i++)
+    {
+        // 추월자 
+        iter = m.find(callings[i]);
+        int temp = iter->second;
+        
+        // 뒤로갈사람
+        iter2 = m2.find(temp-1);
+
+        // 각 map 갱신
+        m[iter->first] = temp - 1;
+        m[iter2->second] = temp;
+
+        m2[temp] = iter2->second;
+        m2[temp - 1] = iter->first;
+        
+    }
+ 
+
+    for (iter2 = m2.begin(); iter2 != m2.end(); iter2++)
+    {
+        answer.push_back(iter2->second);
+    }
+    return answer;
+}
+
+int main()
+{
+    vector<vector<string>> p = { {"mumu", "soe", "poe", "kai", "mine"} };
+    vector<vector<string>> c = { {"kai", "kai", "mine", "mine"} };
+    vector<string> d = solution(p[0], c[0]);
+    vector<string> dab = { "mumu", "kai", "mine", "soe", "poe" };
+
+    bool flag = true;
+    for (int i = 0; i < d.size(); i++)
+    {
+        if (d[i] != dab[i])
+        {
+            flag = false;
+            break;
+        }
+    }
+
+    if (flag == true)
+    {
+        cout << " good " << endl;
+    }
+    else
+    {
+        cout << " eroor " << endl;
+    }
+
+}
+/*
+얀에서는 매년 달리기 경주가 열립니다. 
+해설진들은 선수들이 자기 바로 앞의 선수를 추월할 때 추월한 선수의 이름을 부릅니다. 
+예를 들어 1등부터 3등까지 "mumu", "soe", "poe" 선수들이 순서대로 달리고 있을 때, 
+해설진이 "soe"선수를 불렀다면 2등인 "soe" 선수가 1등인 "mumu" 선수를 추월했다는 것입니다. 
+즉 "soe" 선수가 1등, "mumu" 선수가 2등으로 바뀝니다.
+
+선수들의 이름이 1등부터 현재 등수 순서대로 담긴 문자열 배열 players와 해설진이 부른 이름을 담은 문자열 배열 callings가 매개변수로 주어질 때, 
+경주가 끝났을 때 선수들의 이름을 1등부터 등수 순서대로 배열에 담아 return 하는 solution 함수를 완성해주세요.
+
+제한사항
+5 ≤ players의 길이 ≤ 50,000
+players{i}는 i번째 선수의 이름을 의미합니다.
+players의 원소들은 알파벳 소문자로만 이루어져 있습니다.
+players에는 중복된 값이 들어가 있지 않습니다.
+3 ≤ players{i}의 길이 ≤ 10
+2 ≤ callings의 길이 ≤ 1,000,000
+callings는 players의 원소들로만 이루어져 있습니다.
+경주 진행중 1등인 선수의 이름은 불리지 않습니다.
+입출력 예
+players	callings	result
+		
+입출력 예 설명
+입출력 예 #1
+
+4등인 "kai" 선수가 2번 추월하여 2등이 되고 앞서 3등, 2등인 "poe", "soe" 선수는 4등, 3등이 됩니다. 5등인 "mine" 선수가 2번 추월하여 4등, 3등인 "poe", "soe" 선수가 5등, 4등이 되고 경주가 끝납니다. 1등부터 배열에 담으면 {"mumu", "kai", "mine", "soe", "poe"}이 됩니다.
+*/
